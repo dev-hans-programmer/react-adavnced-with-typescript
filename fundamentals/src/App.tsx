468ce5b17d52fa0baf31bbcc04fdf0733ace3c30
Hasan Ali <hans1998ali@gmail.com>
2023-06-17 22:04:43 +0530

    Added expense list component

--- fundamentals/src/App.tsx
+++ fundamentals/src/App.tsx
@@ -1,19 +1,52 @@
-import ListGroup from './components/ListGroup';
+import { useState } from 'react';
 import './App.css';
-import Alert from './components/Alert';
-import Button from './components/Button';
 import Form from './components/Form';
+import ExpenseList from './expense-tracker/components/ExpenseList';
 
 function App() {
-   const items = ['Kolkata', 'Mumbai', 'New Delhi', 'Bangalore'];
-   const heading = 'List';
+   const staticExpenses = [
+      {
+         id: 1,
+         amount: 50.0,
+         category: 'Food',
+         description: 'Lunch at a local restaurant',
+      },
+      {
+         id: 2,
+         amount: 25.99,
+         category: 'Transportation',
+         description: 'Bus fare for the daily commute',
+      },
+      {
+         id: 3,
+         amount: 12.5,
+         category: 'Entertainment',
+         description: 'Movie ticket for the latest release',
+      },
+      {
+         id: 4,
+         amount: 150.0,
+         category: 'Shopping',
+         description: 'New pair of shoes',
+      },
+      {
+         id: 5,
+         amount: 30.0,
+         category: 'Utilities',
+         description: 'Internet bill for the month',
+      },
+   ];
 
-   function handleSelectItem(item: string) {
-      console.log(item);
+   const [expenses, setExpenses] = useState(staticExpenses);
+
+   function handleDelete(id: number) {
+      setExpenses((prev) => prev.filter((item) => item.id !== id));
    }
+
    return (
       <div>
-         <Form />
+         {/* <Form /> */}
+         <ExpenseList expenses={expenses} onDelete={handleDelete} />
       </div>
    );
 }
--- fundamentals/src/components/Form.tsx
+++ fundamentals/src/components/Form.tsx
@@ -1,16 +1,27 @@
 import { FieldValues, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 
-interface IUser {
-   name: string;
-   age: number;
-}
+const schema = z.object({
+   name: z.string().min(3, { message: 'Name must be 3 characters long.' }),
+   age: z
+      .number({ invalid_type_error: 'Age is required.' })
+      .min(18, { message: 'Age must be at least 18.' }),
+});
+
+type IUser = z.infer<typeof schema>;
+
+// interface IUser {
+//    name: string;
+//    age: number;
+// }
 
 function Form() {
    const {
       register,
       handleSubmit,
-      formState: { errors },
-   } = useForm<IUser>();
+      formState: { errors, isValid },
+   } = useForm<IUser>({ resolver: zodResolver(schema) });
 
    const onSubmit = (data: FieldValues) => {
       console.log(data);
@@ -29,7 +40,7 @@ function Form() {
                {...register('name', { required: true, minLength: 3 })}
             />
             {errors.name && (
-               <p className='text-danger'>Input field is not valid</p>
+               <p className='text-danger'>{errors.name.message}</p>
             )}
          </div>
          <div className='mb-3'>
@@ -40,13 +51,13 @@ function Form() {
                id='age'
                type='number'
                className='form-control'
-               {...register('age', { required: true })}
+               {...register('age', { required: true, valueAsNumber: true })}
             />
-            {errors.age && (
-               <p className='text-danger'>Input field is not valid</p>
-            )}
+            {errors.age && <p className='text-danger'>{errors.age.message}</p>}
          </div>
-         <button className='btn btn-primary'>Submit</button>
+         <button disabled={!isValid} className='btn btn-primary'>
+            Submit
+         </button>
       </form>
    );
 }
