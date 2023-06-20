28a42c14766a1fb4a0fdfbe937b2640da7592ce1
Hasan Ali <hans1998ali@gmail.com>
2023-06-20 18:27:27 +0530

    Added dark mode in game hub project

---
+++ game-hub/src/App.tsx
@@ -0,0 +1,28 @@
+import { Button, Grid, GridItem, Show } from '@chakra-ui/react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+   return (
+      <Grid
+         templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`,
+         }}
+      >
+         <GridItem area='nav'>
+            <Navbar />
+         </GridItem>
+         <Show above='lg'>
+            <GridItem area='aside' bg='blue'>
+               Aside
+            </GridItem>
+         </Show>
+         <GridItem area='main' bg='red'>
+            Main
+         </GridItem>
+      </Grid>
+   );
+}
+
+export default App;
